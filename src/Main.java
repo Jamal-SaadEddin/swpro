234db2d0cda6b729fe957d3ee4b344ebd7dc9c1d
Jamaloooo <jamalsa3d2001@gmail.com>
2022-05-09 15:43:14 +0300

    new1

--- src/Main.java
+++ src/Main.java
@@ -128,7 +128,7 @@ public class Main extends javax.swing.JFrame {
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new vacations().setVisible(true);
+                new Main().setVisible(true);
                
             }
         });
